928996080fc75f699b36424e154950743489c128
cheetah676 <jake.mordan@gmail.com>
2020-09-20 11:31:07 -0700

    threaded greeter done, thread pool ip, very confusing, swimming ip

--- src/_03_Threaded_Greeting/ThreadedGreeter.java
+++ src/_03_Threaded_Greeting/ThreadedGreeter.java
@@ -4,14 +4,22 @@ public class ThreadedGreeter implements Runnable {
 int threadNum;
 	public ThreadedGreeter(int i) {
 		// TODO Auto-generated constructor stub
+		threadNum=i;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		Thread n=new Thread();
 		System.out.println("Hello from thread number: "+threadNum);
-		if(threadNum<=50) {
-			
+		if(threadNum<50) {
+			n=new Thread(new ThreadedGreeter(threadNum+=1));
+			n.start();
+			try {
+				n.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,30 @@
 package _04_Thread_Pool;
 
-public class ThreadPool {
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class ThreadPool {
+Thread[] t;
+ConcurrentLinkedQueue<Task> taskQueue;
+
+ThreadPool(int totalThreads){
+	t=new Thread[totalThreads];
+	for(int i=0; i<t.length; i++) {
+		t[i]=new Worker();
+		taskQueue=new ConcurrentLinkedQueue<Task>();
+	}
+}
+void addTask(Task task) {
+	taskQueue.add(task);
+}
+void start() {
+	for(int i=0; i<t.length; i++) {
+	t[i].start();
+	try {
+		t[i].join();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	}
+}
 }
